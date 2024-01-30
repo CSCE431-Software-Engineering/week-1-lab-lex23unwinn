8dbe1c705abc2c32bffa7c2f1e67922a59d51a17
lex23unwin <lex23unwin@gmail.com>
2024-01-29 18:20:52 -0600

    tests for not-yet-written attributes

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -8,9 +8,24 @@ RSpec.describe 'Creating a book', type: :feature do
     scenario 'with valid inputs' do 
         visit new_book_path
         fill_in "book[title]", with: 'Meditations'
+        fill_in "book[author]", with: 'Marcus C.H.A.D. Aurelius'
+        fill_in "book[price]", with: '19.99'
+        fill_in "book[date]", with: '2002-06-26'
         click_on 'Create Book'
         visit books_path
-        expect(page).to have_content('harry potter')
+        expect(page).to have_content('Meditations')
+        expect(page).to have_content('Marcus C.H.A.D Aurelius')
+        expect(page).to have_content("19.99")
+        expect(page).to have_content("2002-06-26")
+    end
+
+    scenario 'without valid inputs' do
+        visit new_book_path
+        click_on 'Create Book'
+        expect(page).to have_content('Title can\'t be blank')
+        expect(page).to have_content('Author can\'t be blank')
+        expect(page).to have_content('Price can\'t be blank')
+        expect(page).to have_content('Published date can\'t be blank')
     end
 end
 
