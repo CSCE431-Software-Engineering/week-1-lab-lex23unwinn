3f53e5f75676f88dbe7ddd80a7f75dc711eed7c4
lex23unwin <lex23unwin@gmail.com>
2024-02-16 14:11:31 -0600

    new view, new routes for user_books index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   resources :user_books
   resources :users
+
+  root 'user_books#index'
+
   resources :books do 
     member do 
       get 'delete'
       #member code block for the extra one i need, member not collection
     end
   end 
-  root "books#index"
 end
 
 
